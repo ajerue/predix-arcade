7833f58a2d00cc2a37d22c7fa6b94cb4d6906f5f
futuregarnet <futuregarnet@gmail.com>
2018-06-20 04:58:40 -0400

    chore: Configure for Predix deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_ROM_URL: "https://d3cto2l652k3y0.cloudfront.net/",
+  BASE_ROM_URL: "https://predix-jsnes.run.aws-usw02-pr.ice.predix.io/roms/",
   GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
   SENTRY_URI: process.env.REACT_APP_SENTRY_URI
 };
